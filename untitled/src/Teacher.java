4be244f32bae7e2d199e321959c2a34c18258b82
xjy <1498957049@qq.com>
2019-11-07 08:40:36 +0800

    忽略

---
+++ untitled/src/Teacher.java
@@ -0,0 +1,30 @@
+public class Teacher {
+    public static void main(String[] args) {
+//
+        Teacher t=new Teacher();
+        t.sDE(1);
+
+    }
+    public void sDE(int a){
+
+        switch (a){
+            case 1:{
+                System.out.println("我");
+                return ;
+            }
+            case 2:
+                System.out.println("在");
+                break;
+            case 3:
+                System.out.println("想");
+                break;
+
+
+
+        }
+
+
+    }
+
+}
+
